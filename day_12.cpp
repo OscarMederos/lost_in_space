6a4204caf52a422ea4001b2f081a435598affc33
osmed086 <OscarMederos@users.noreply.github.com>
2022-07-25 16:17:26 -0400

    Update day_12.cpp

--- day_12.cpp
+++ day_12.cpp
@@ -8,10 +8,10 @@ byte rowPins[ROWS] = {5, 4, 3, 2};
 byte colPins[COLS] = {6, 7, 8, 9};
  
 char buttons[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},  // 1st row
-  {'4', '5', '6', 'B'},  // 2nd row
-  {'7', '8', '9', 'C'},  // 3rd row
-  {'*', '0', '#', 'D'}   // 4th row
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
  
 Keypad myAwesomePad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
